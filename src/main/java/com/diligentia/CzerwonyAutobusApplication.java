786adf2ecff8e05ca2e01f054c2f1396de364bd9
rwichrowski <rwichrowski@efigence.com>
2018-01-22 06:40:04 +0100

    Zmiana pakietów porządki część 2

--- src/main/java/com/diligentia/CzerwonyAutobusApplication.java
+++ src/main/java/com/diligentia/CzerwonyAutobusApplication.java
@@ -2,22 +2,21 @@ package com.diligentia;
 
 import javax.sql.DataSource;
 
-import com.diligentia.model.ExpenceCategory;
-import com.diligentia.model.Expense;
+import com.diligentia.model.Recipe;
+import com.diligentia.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.diligentia.repository.SystemRepository;
-
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 //@SpringBootApplication
-//@EnableJpaRepositories("com.diligentia.czerwony.repository")
-//@EntityScan("com.diligentia.czerwony.model")
+//@EnableJpaRepositories("com.diligentia.repository")
+//@EntityScan("com.diligentia.model")
 public class CzerwonyAutobusApplication implements CommandLineRunner {
 
     @Qualifier("dataSource")
@@ -25,7 +24,7 @@ public class CzerwonyAutobusApplication implements CommandLineRunner {
     DataSource dataSource;
 
     @Autowired
-    SystemRepository systemRepository;
+    RecipeRepository recipeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CzerwonyAutobusApplication.class, args);
@@ -34,28 +33,24 @@ public class CzerwonyAutobusApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         createTestData();
-        Iterable<Expense> systemlist = systemRepository.findAll();
+        Iterable<Recipe> systemlist = recipeRepository.findAll();
         System.out.println("radek");
-        for (Expense systemmodel : systemlist) {
+        for (Recipe systemmodel : systemlist) {
             System.out.println("Here is a system: " + systemmodel.toString());
         }
     }
 
     private void createTestData() {
-        Expense expense;
+        Recipe recipe;
 
-        expense = new Expense();
-        expense.setName("Apteka");
-        expense.setDate(LocalDate.now());
-        expense.setAmount(BigDecimal.TEN);
-        expense.setExpenceCategory(ExpenceCategory.HEALTH);
-        systemRepository.save(expense);
+        recipe = new Recipe();
+        recipe.setName("Apteka");
+        recipe.setCreationDate(LocalDate.now());
+        recipeRepository.save(recipe);
 
-        expense = new Expense();
-        expense.setName("Biedronka");
-        expense.setDate(LocalDate.now());
-        expense.setAmount(BigDecimal.valueOf(13.9));
-        expense.setExpenceCategory(ExpenceCategory.FOOD);
-        systemRepository.save(expense);
+        recipe = new Recipe();
+        recipe.setName("Biedronka");
+        recipe.setCreationDate(LocalDate.now());
+        recipeRepository.save(recipe);
     }
 }
