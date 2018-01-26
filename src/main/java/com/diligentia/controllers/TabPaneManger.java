45df63b848469a21f66eb2d4138f387e64120865
rwichrowski <rwichrowski@efigence.com>
2018-01-26 08:09:21 +0100

    Listener do zmiany zakladek

--- src/main/java/com/diligentia/controllers/TabPaneManger.java
+++ src/main/java/com/diligentia/controllers/TabPaneManger.java
@@ -1,19 +1,31 @@
 package com.diligentia.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class TabPaneManger {
 
-    private final ConsoleTabController consoleTabController;
     private final LoggerTabController loggerTabController;
 
+    @FXML
+    TabPane tabPane;
+
     @Autowired
-    public TabPaneManger(ConsoleTabController consoleTabController, LoggerTabController loggerTabController) {
-        this.consoleTabController = consoleTabController;
+    public TabPaneManger(LoggerTabController loggerTabController) {
         this.loggerTabController = loggerTabController;
+    }
+
+    public void initialize() {
+        tabPane.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            System.err.println("It's works");
+        });
+
     }
 
     public TextArea getVisualLog() {
